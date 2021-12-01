4d3164df299d975dc8927340f408155d924325a0
aakashpotdar <potdaraakash@gmail.com>
2021-12-01 22:57:06 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using CsvHelper;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace IndianStateCesusAnalyzer
+{
+    public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            ImportDataFromCsv obj = new ImportDataFromCsv("C:/Users/AKASH/source/repos/Cesus Analyzer/IndianStateCesusAnalyzer/IndianStateCesusAnalyzer/CensusData.csv");
+            obj.ImportData();
+        }
+    }
+}
